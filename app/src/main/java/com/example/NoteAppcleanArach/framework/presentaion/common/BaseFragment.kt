689454ca1aff35573dcb11ad7d7fa484a59bffaa
hossam <hossamkotp89@gmail.com>
2020-07-08 13:57:54 +0200

    create project and adding some fragments

---
+++ app/src/main/java/com/example/NoteAppcleanArach/framework/presentaion/common/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.example.NoteAppcleanArach.framework.presentaion.common
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/NoteAppcleanArach/framework/util/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.NoteAppcleanArach.framework.util
+
+object Constants {
+}
