0e93b36f310abd16339a9f501ed179e9d28f3708
hossam <hossamkotp89@gmail.com>
2020-07-14 18:36:11 +0200

    adding GetNumNotes.kt usecase

--- app/src/main/java/com/example/NoteAppcleanArach/businees/usecases/notelist/GetNumNotes.kt
+++ app/src/main/java/com/example/NoteAppcleanArach/businees/usecases/notelist/GetNumNotes.kt
@@ -1,5 +1,45 @@
 package com.example.NoteAppcleanArach.businees.usecases.notelist
 
-class GetNumNotes {
+import com.example.NoteAppcleanArach.businees.data.cache.abstraction.NoteCacheDataSource
+import com.example.NoteAppcleanArach.businees.data.cache.util.CacheResponseHandler
+import com.example.NoteAppcleanArach.businees.data.util.safeCacheCall
+import com.example.NoteAppcleanArach.businees.doman.state.*
+import com.example.NoteAppcleanArach.framework.presentaion.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetNumNotes(
+    private val noteCacheDataSource: NoteCacheDataSource
+) {
+
+    fun getTotalNumOfNotes(stateEvent: StateEvent): Flow<DataState<NoteListViewState>?> = flow {
+
+        val cacheObject = safeCacheCall(IO) {
+            noteCacheDataSource.getNumberOfNotes()
+        }
+
+        val response = object : CacheResponseHandler<NoteListViewState, Int>(
+            response = cacheObject,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: Int): DataState<NoteListViewState> =
+                DataState.data(
+                    response = Response(
+                        messageType = MessageType.Success(),
+                        uiComponentType = UIComponentType.None(),
+                        message = GET_NUM_NOTES_SUCCESS
+                    ),
+                    data = NoteListViewState(pageNumber = resultObj),
+                    stateEvent = stateEvent
+                )
+
+        }.getResult()
+        emit(response)
+    }
+
+    companion object{
+        const val GET_NUM_NOTES_SUCCESS = "successfully retrieval the num of notes from the cache"
+    }
 
 }
